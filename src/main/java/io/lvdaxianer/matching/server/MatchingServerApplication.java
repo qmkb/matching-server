e5257b0af78cea21a9db7c838002777fa88bd2d4
浩浩 李 <15064044877@163.com>
2025-09-10 07:08:05 +0800

    feat: pom.xml 相关的依赖添加

---
+++ src/main/java/io/lvdaxianer/matching/server/MatchingServerApplication.java
@@ -0,0 +1,17 @@
+package io.lvdaxianer.matching.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author lvdaxianer
+ */
+@SpringBootApplication
+public class MatchingServerApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MatchingServerApplication.class, args);
+    }
+}
